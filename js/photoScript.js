9602f288b21b85cc0bf475d7581a3e0db378f18d
dan.michaelis <dan.michaelis@perficient.com>
2024-05-13 19:52:11 -0400

    Making all divs w3-responsive to fix page width consistency problems

--- js/photoScript.js
+++ js/photoScript.js
@@ -1,38 +1,58 @@
 let images = [
-  {name:'bull.jpg',
-    pos: 'One'},
-  {name:'dringkingHabits.jpg',
-  pos: 'Two'},
-  {name:'group.jpg',
-  pos: 'Three'},
-  {name:'gypsy.jpg',
-  pos: 'Four'},
-  {name:'hartman.jpg',
-  pos: 'Five'},
-  {name:'jyon.jpg',
-  pos: 'Six'},
-  {name:'liverpool.jpg',
-  pos: 'Seven'},
-  {name:'mainImage.jpg',
-  pos: 'Eight'},
-  {name:'miracleWorker.jpg',
-  pos: 'Nine'},
-  {name:'oz.jpg',
-  pos: 'Ten'}
+  {
+    name: 'bull.jpg',
+    pos: 'One'
+  },
+  {
+    name: 'dringkingHabits.jpg',
+    pos: 'Two'
+  },
+  {
+    name: 'group.jpg',
+    pos: 'Three'
+  },
+  {
+    name: 'gypsy.jpg',
+    pos: 'Four'
+  },
+  {
+    name: 'hartman.jpg',
+    pos: 'Five'
+  },
+  {
+    name: 'jyon.jpg',
+    pos: 'Six'
+  },
+  {
+    name: 'liverpool.jpg',
+    pos: 'Seven'
+  },
+  {
+    name: 'mainImage.jpg',
+    pos: 'Eight'
+  },
+  {
+    name: 'miracleWorker.jpg',
+    pos: 'Nine'
+  },
+  {
+    name: 'oz.jpg',
+    pos: 'Ten'
+  }
 
 ];
 
-for (let t = 0; t <= images.length - 1;t++){
+for (let t = 0; t <= images.length - 1; t++) {
   let tPos = t + 1
-  let cnt = images.length 
+  let cnt = images.length
   // $('#imgZone').append('<div class="mySlides fade" ><div class="numbertext">' + tPos + ' / ' + cnt +'</div><img id="imgCar" src="img/photos/' + images[t] + '"><div class="text">text</div></div>');
   // $('#imgZone').append('<a class="carousel-item" href="#"><img class= "carImg" src="img/photos/' + images[t].name + '"></a>')
-  if (t === 0){
-    $('#imgZone').append('<li class="slide" data-active ><img src="img/photos/' + images[t].name + '" alt="' + images[t].pos + '"></li>')
+  if (t === 0) {
+    $('#imgZone').append('<li class="slide" data-active ><img style="width: 100%" src="img/photos/' + images[t].name + '" alt="' + images[t].pos + '"></li>')
 
   } else {
 
-    $('#imgZone').append('<li class="slide"><img src="img/photos/' + images[t].name + '" alt="' + images[t].pos + '"></li>')
+    $('#imgZone').append('<li class="slide"><img style="width: 100%" src="img/photos/' + images[t].name + '" alt="' + images[t].pos + '"></li>')
   }
 
 
@@ -51,7 +71,7 @@ buttons.forEach(button => {
     const slides = button.closest('[data-carousel]').querySelector('[data-slides]')
     const activeSlide = slides.querySelector('[data-active]')
     let newIndex = [...slides.children].indexOf(activeSlide) + offset
-    if (newIndex < 0 ) newIndex = slides.children.length - 1
+    if (newIndex < 0) newIndex = slides.children.length - 1
     if (newIndex >= slides.children.length) newIndex = 0
 
 
--- js/workshop.js
+++ js/workshop.js
@@ -1,13 +1,13 @@
 // Hyphens need to be replaced with &#45;
 // apostrophe needs to be replaced with &#39;
 
-let beginners = ['Beginner&#39;s Camp Offerings','(one week in length)','June 10th &#45; June 14th','and','June 24th &#45; June 28th'];
-let advanced = ['Advanced Camp Offering','(two weeks in length)','July 15th &#45; July 26th'];
+let beginners = ['Beginner&#39;s Camp Offerings (one week in length)', 'June 10th &#45; June 14th and June 24th &#45; June 28th'];
+let advanced = ['Advanced Camp Offering (two weeks in length)', 'July 15th &#45; July 26th'];
 
-for (let a = 0; a<= advanced.length - 1;a++){
-    $('#advGrp').append("<li><p>"+advanced[a]+"</p></li>")
+for (let a = 0; a <= advanced.length - 1; a++) {
+    $('#advGrp').append("<li><p>" + advanced[a] + "</p></li>")
 };
 
-for (let b = 0; b<= beginners.length - 1;b++){
-    $('#begGrp').append("<li><p>"+beginners[b]+"</p></li>")
+for (let b = 0; b <= beginners.length - 1; b++) {
+    $('#begGrp').append("<li><p>" + beginners[b] + "</p></li>")
 };
